c52766c24c4fa932a0df6d8f0bada17e72f0a7ae
Micro do Guto <gustavoguesser@gmail.com>
2021-06-28 16:22:58 -0300

    Alterado gestor de dependencias para pipenv close #11

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -2,7 +2,8 @@ class Enviador:
     def enviar(self, remetente, destinatario, titulo, corpo):
         if '@' not in remetente:
             raise EmailInvalido(f'Email de remetente invalido: {remetente}')
-            return remetente
+        return remetente
+
 
 class EmailInvalido(Exception):
     pass
